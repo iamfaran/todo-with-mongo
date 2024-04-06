139bda864f87bdaf2002d4b0e34cbb9f1c2aabcf
Faran Javed <faran1997@outlook.com>
2024-04-07 01:10:35 +0500

    add signin with google btn

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { SignInGoogle } from "@/components/SignInGoogle";
 export default function Home() {
   console.log("Home");
   return (
-    <div className="bg-gray-100 dark:bg-slate-900 h-screen">
-      Auth Page
-      <SignInGoogle />
+    <div className="bg-gray-100 dark:bg-slate-900 h-screen flex justify-center items-center">
+      <main>
+        <h1 className="text-center mb-3">Todos</h1>
+        <SignInGoogle />
+      </main>
     </div>
   );
 }
