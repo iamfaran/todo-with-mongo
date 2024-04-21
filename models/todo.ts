83e811e59f0d7e76089e7580bfb4975a169137a1
Faran Javed <faran1997@outlook.com>
2024-04-21 16:40:17 +0500

    update schema

--- models/todo.ts
+++ models/todo.ts
@@ -10,6 +10,11 @@ const todoSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     timestamps: true,
