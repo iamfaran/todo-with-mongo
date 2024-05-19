95becc103b62b433ea6fa4d9e48dfcda8334f1b7
Faran Javed <faran1997@outlook.com>
2024-05-19 19:48:39 +0500

    fix theme icon on nested routes

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,7 +3,7 @@ import SunMoon from "@/components/SunMoon";
 
 export default function TodosPage() {
   return (
-    <div className="bg-gray-100 dark:bg-slate-900 h-screen relative">
+    <div className="bg-gray-100 dark:bg-slate-900 h-screen">
       <Todos />
     </div>
   );
