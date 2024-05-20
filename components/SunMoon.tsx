c2d3e10315b3ebd3a6e17b3cf28e92e23d006b0b
Faran Javed <faran1997@outlook.com>
2024-05-20 20:12:47 +0500

    update theme toggle

--- components/SunMoon.tsx
+++ components/SunMoon.tsx
@@ -10,11 +10,27 @@ function SunMoon() {
   // if Dark mode is true then show the sun icon
   // else show the moon icon
 
+  const themeToggle = async () => {
+    try {
+      const response = await fetch("/api/theme", {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ darkMode: !state.darkMode }),
+      });
+
+      if (response.ok) {
+        dispatch({ type: "TOGGLE_THEME" });
+      } else {
+        console.error("Error updating theme on server:", response.statusText);
+        // (Optional) Show error feedback to the user
+      }
+    } catch (error) {
+      console.error("Error updating theme:", error);
+      // (Optional) Show error feedback to the user
+    }
+  };
   return (
-    <div
-      onClick={() => dispatch({ type: "TOGGLE_THEME" })}
-      className="absolute left-5 top-5"
-    >
+    <div onClick={themeToggle} className="absolute left-5 top-5">
       {/* <MdDarkMode className="text-4xl" /> */}
       {state.darkMode ? (
         <MdLightMode className="text-4xl" />
